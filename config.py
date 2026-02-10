df45fe3de15b148e149fd0457e4b91f2c65ff8f7
Avroumiii <avroumi97@gmail.com>
2026-02-10 12:02:06 +0200

    change config in fuction , add fuction in checks protocol_version , suspecious_per_id

--- config.py
+++ config.py
@@ -1,2 +1,10 @@
-SIZE_WARNING = 5000
-PORTS_WARNING = ("22","23","3389")
+from datetime import time
+
+
+
+SIZE_WARNING = lambda s : s > 5000
+PORTS_WARNING = lambda w : w in ("22","23","3389")
+NIGHT_ACTIVITY = lambda t: 0 <= int(t.split(" ")[1][:2]) < 6
+EXTERNAL_IP = lambda ip : not  ip.startswith(("10.", "192.168."))
+
+
